72aae173012e0b7f3d4f50c229127531ae1351fe
Ange230700 <kouakouangeericstephane@gmail.com>
2025-03-21 14:26:47 +0100

    supprime un menu

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
 # Adalicious
 
-## POST /user/login
+## POST /user/login : connecte le client
 
 ![connexion](./back/public/connexion.png)
 
-## GET /menus/list
+## GET /menus/list : récupère la liste des menus
 
 ![liste_menus](./back/public/getMenusList.png)
+
+## GET /menus/{menu_id} : récupère un menu dans la liste
+
+![one_menu](./back/public/oneMenu.png)
+
+## PUT /menus/{menu_id} : modifie un menu
+
+![edited_menu](./back/public/editedMenu.png)
+
+## DELETE /menus/{menu_id} : supprime un menu
+
+![deleted_menu](./back/public/deletedMenu.png)
+
+
--- back/routes/menu.js
+++ back/routes/menu.js
@@ -18,14 +18,58 @@ router.get("/list", async function (req, res, next) {
 router.get("/:menu_id", async function (req, res, next) {
   try {
     const { menu_id } = req.params;
+    if (!menu_id) {
+      res.sendStatus(404).send("Menu non trouvé.");
+    }
     const [[menu]] = await databaseClient.query(
       `SELECT * FROM Menu WHERE menu_id = ?`,
       [menu_id]
     );
-    if (!menu) {
+    res.json(menu);
+  } catch (e) {
+    console.error(e);
+    next(e);
+  }
+});
+
+router.put("/:menu_id", async function (req, res, next) {
+  try {
+    const { menu_id } = req.params;
+    if (!menu_id) {
       res.sendStatus(404).send("Menu non trouvé.");
     }
-    res.json(menu);
+    const { menu_title, menu_description, menu_image } = req.body;
+    if (!menu_title || !menu_description || !menu_image) {
+      res.sendStatus(400).send("Tous les champs sont requis.");
+    }
+    const [isMenuUpdated] = await databaseClient.query(
+      `UPDATE Menu SET menu_title = ?, menu_description = ?, menu_image = ? WHERE menu_id = ?`,
+      [menu_title, menu_description, menu_image, menu_id]
+    );
+    if (!isMenuUpdated) {
+      res.sendStatus(400).send("Erreur lors de la modification du menu");
+    }
+    res.status(200).send("Menu modifié.");
+  } catch (e) {
+    console.error(e);
+    next(e);
+  }
+});
+
+router.delete("/:menu_id", async function (req, res, next) {
+  try {
+    const { menu_id } = req.params;
+    if (!menu_id) {
+      res.sendStatus(404).send("Menu non trouvé.");
+    }
+    const [isMenuDeleted] = await databaseClient.query(
+      `DELETE FROM Menu WHERE menu_id = ?`,
+      [menu_id]
+    );
+    if (!isMenuDeleted) {
+      res.sendStatus(400).send("Erreur lors de la suppression du menu");
+    }
+    res.status(200).send("Menu supprimé.");
   } catch (e) {
     console.error(e);
     next(e);
